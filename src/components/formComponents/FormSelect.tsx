e198ef26f3aadf542f2b3d4b29c9345925484ecb
anviiiijain <anvijain250102@gmail.com>
2023-02-14 19:21:15 +0530

    Bug Resolved - form select overlap

--- src/components/formComponents/FormSelect.tsx
+++ src/components/formComponents/FormSelect.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { useTheme } from "@chakra-ui/react";
-import FromWrapper from "./FormWrapper";
-import { IFormInputProps } from "@src/interface/forms";
-import ReactSelect, { Props } from "react-select";
+import React, { useEffect, useState } from 'react'
+import { useTheme } from '@chakra-ui/react'
+import FromWrapper from './FormWrapper'
+import { IFormInputProps } from '@src/interface/forms'
+import ReactSelect, { Props } from 'react-select'
 
 interface IFormSelectProps
-  extends Omit<IFormInputProps, "inputProps" | "type" | "onChange" | "onBlur"> {
-  options: { label: string; value: string }[] | any;
-  selectProps?: Props;
-  onChange?: any;
-  onBlur?: any;
+  extends Omit<IFormInputProps, 'inputProps' | 'type' | 'onChange' | 'onBlur'> {
+  options: { label: string; value: string }[] | any
+  selectProps?: Props
+  onChange?: any
+  onBlur?: any
 }
 
 const FormSelect: React.FC<IFormSelectProps> = ({
@@ -27,14 +27,26 @@ const FormSelect: React.FC<IFormSelectProps> = ({
   wrapperProps = {},
   options,
 }) => {
-  const theme = useTheme();
+  const theme = useTheme()
 
   const handleChange = (value: any) => {
-    onChange && onChange(name, value?.value);
-  };
+    onChange && onChange(name, value?.value)
+  }
   const handleBlur = () => {
-    onBlur && onBlur(name, true);
-  };
+    onBlur && onBlur(name, true)
+  }
+
+  const [menuPortalTarget, setMenuPortalTarget] = useState<HTMLElement | null>(
+    null
+  )
+
+  //Problem: select options were being rendered behind other elements, Solution: set the menuPortalTarget to document.body when the document is defined
+
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      setMenuPortalTarget(document.body)
+    }
+  }, [])
 
   return (
     <FromWrapper
@@ -52,49 +64,51 @@ const FormSelect: React.FC<IFormSelectProps> = ({
         onChange={handleChange}
         onBlur={handleBlur}
         options={options}
+        menuPortalTarget={menuPortalTarget}
+        menuPosition={'fixed'}
         // styles
         styles={{
           container: (base) => ({
             ...base,
-            width: "100%",
-            minWidth: "none",
-            height: "auto",
-            maxHeight: "none",
-            minHeight: "none",
+            width: '100%',
+            minWidth: 'none',
+            height: 'auto',
+            maxHeight: 'none',
+            minHeight: 'none',
           }),
           control: (base, { isFocused }) => ({
             ...base,
-            width: "100%",
-            minWidth: "272px",
-            height: "45px",
+            width: '100%',
+            minWidth: '272px',
+            height: '45px',
             border: isFocused
               ? `1px solid ${theme.colors.primary}`
               : error
               ? `1px solid ${theme.colors.errorRed}`
-              : "1px solid #c0bcd7",
+              : '1px solid #c0bcd7',
             backgroundColor: theme.colors.inputBg,
-            borderRadius: "10px",
-            fontSize: ".875rem",
-            fontWeight: "500",
-            "&:hover": {
+            borderRadius: '10px',
+            fontSize: '.875rem',
+            fontWeight: '500',
+            '&:hover': {
               border: `1px solid ${theme.colors.primary}`,
             },
           }),
           valueContainer: (base) => ({
             ...base,
-            paddingLeft: "20px",
+            paddingLeft: '20px',
           }),
           option: (base, { isFocused }) => ({
             ...base,
-            fontSize: ".875rem",
-            fontWeight: "500",
+            fontSize: '.875rem',
+            fontWeight: '500',
           }),
         }}
         {...selectProps}
       />
       {children}
     </FromWrapper>
-  );
-};
+  )
+}
 
-export default FormSelect;
+export default FormSelect
